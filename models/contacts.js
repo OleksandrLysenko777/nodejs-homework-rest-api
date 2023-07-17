41d7d46d6ec51cc0200f047903927e2c14d084d8
OleksandrLysenko777 <direst2010@gmail.com>
2023-07-17 03:24:22 +0200

    hw node js

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,114 @@
-// const fs = require('fs/promises')
+const fs = require("fs").promises;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const contactsPath = path.format({
+  root: "/ignored",
+  dir: "models",
+  base: "contacts.json",
+});
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  try {
+    const contactsData = await fs.readFile(contactsPath, "utf-8");
+    let contacts = JSON.parse(contactsData);
 
-const getContactById = async (contactId) => {}
+    if (!Array.isArray(contacts)) {
+      contacts = [];
+    }
 
-const removeContact = async (contactId) => {}
+    return contacts;
+  } catch (error) {
+    return [];
+  }
+};
 
-const addContact = async (body) => {}
+const getContactById = async (contactId) => {
+  try {
+    const contactsData = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(contactsData);
+    const contact = contacts.find((c) => c.id === contactId);
+    return contact;
+  } catch (error) {
+    throw new Error("Could not get the contact.");
+  }
+};
 
-const updateContact = async (contactId, body) => {}
+const removeContact = async (contactId) => {
+  try {
+    let isContact = false;
+    const contactsData = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(contactsData);
+
+    const updatedContacts = contacts.filter((contact) => {
+      if (contact.id === contactId) {
+        isContact = true;
+      }
+      return contact.id !== contactId;
+    });
+
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+
+    return isContact;
+  } catch (error) {
+    throw new Error("Could not remove the contact.");
+  }
+};
+
+const addContact = async (body) => {
+  try {
+    const contactId = uuidv4();
+    const newContact = {
+      id: contactId,
+      name: body.name,
+      email: body.email,
+      phone: body.phone,
+    };
+
+    let contacts = [];
+
+    try {
+      const contactsData = await fs.readFile(contactsPath, "utf-8");
+      contacts = JSON.parse(contactsData);
+    } catch (error) {}
+
+    const updatedContacts = [...contacts, newContact];
+
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+
+    return newContact;
+  } catch (error) {
+    throw new Error("Could not add the contact.");
+  }
+};
+
+const updateContact = async (contactId, body) => {
+  try {
+    let isContact = false;
+    const contactsData = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(contactsData);
+
+    const updatedContacts = contacts.map((contact) => {
+      if (contact.id === contactId) {
+        isContact = true;
+        return { ...contact, ...body };
+      }
+      return contact;
+    });
+
+    if (!isContact) {
+      return null;
+    }
+
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+
+    const updatedContact = updatedContacts.find(
+      (contact) => contact.id === contactId
+    );
+    return updatedContact;
+  } catch (error) {
+    throw new Error("Could not update the contact.");
+  }
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +116,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
---
+++ validations/contactsValidation.js
@@ -0,0 +1,38 @@
+const Joi = require("joi");
+
+function validateContact(req, res, next) {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({ message: "missing fields" });
+  }
+  const contactValidation = Joi.object({
+    name: Joi.string()
+      .pattern(/^[A-Za-z\s]+$/)
+      .required()
+      .messages({
+        "string.pattern.base": `name should contain first name and last name`,
+        "any.required": `missing required name field`,
+      }),
+    email: Joi.string().email().required().messages({
+      "string.email": `email should be a valid email`,
+      "any.required": `missing required email field`,
+    }),
+    phone: Joi.string()
+      .pattern(
+        /^([+][0-9]{0,4})?[\s]?([(][0-9]{1,3}[)])?[\s]?[0-9]{2,3}[-\s]?[0-9]{2,3}[-\s]?[0-9]{2,4}$/
+      )
+      .required()
+      .messages({
+        "string.pattern.base": `phone is incorrect`,
+        "any.required": `missing required phone field`,
+      }),
+  });
+
+  const { error } = contactValidation.validate(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  next();
+}
+
+module.exports = { validateContact };
